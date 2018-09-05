eb91c4fed005c98a8aa706e1c5be417538390bbc
rafaelalcindo <rafael_alcindo@hotmail.com>
2018-09-05 19:11:08 -0300

    next step

--- src/components/ListItem/ListItem.js
+++ src/components/ListItem/ListItem.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native';
 
 const listItem = (props) => (
-    <View style={styles.listItem} >
-        <Text> {props.placeName} </Text>
+    <View style={styles.listItem}>
+        <Text>{props.placeName}</Text>
     </View>
 );
 
 const styles = StyleSheet.create({
     listItem: {
-        width: '100%',
+        width: "100%",
         marginBottom: 5,
         padding: 10,
         backgroundColor: "#eee"
