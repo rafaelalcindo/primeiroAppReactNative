02bda53a4cbae1e220d79cb33a1e3de4487a7f9d
rafaelalcindo <rafael_alcindo@hotmail.com>
2018-09-09 22:47:23 -0300

    fim dos testes

--- src/components/PlaceList/PlaceList.js
+++ src/components/PlaceList/PlaceList.js
@@ -1,21 +1,28 @@
-import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import React from "react";
+import { StyleSheet, FlatList } from "react-native";
 
-import ListItem from '../ListItem/ListItem';
+import ListItem from "../ListItem/ListItem";
 
 const placeList = props => {
-    const placesOutput = props.places.map((place, i) => (
-        <ListItem key={i} placeName={place} />
-      ));
-    return (
-        <View style={styles.listContainer}>{placesOutput}</View>
-    );
+  return (
+    <FlatList
+      style={styles.listContainer}
+      data={props.places}
+      renderItem={(info) => (
+        <ListItem
+          placeName={info.item.name}
+          placeImage={info.item.image}
+          onItemPressed={() => props.onItemSelected(info.item.key)}
+        />
+      )}
+    />
+  );
 };
 
 const styles = StyleSheet.create({
-    listContainer: {
-      width: "100%"
-    }
+  listContainer: {
+    width: "100%"
+  }
 });
 
 export default placeList;
